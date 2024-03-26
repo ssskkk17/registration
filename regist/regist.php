a48b6c3998bd79a7235ed45005dd1a22321b4c45
ssskkk17 <not36verstile@gmail.com>
2024-03-26 21:58:09 +0900

    first push

---
+++ regist/regist.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="utf-8">
+    <title>regist画面</title>
+    <style>
+        header {
+            background-color: black;
+            height: 50px;
+            width: 100%;
+            position: relative;
+        }
+        header ul li {
+            color: white;
+            float: left;
+            font-size: 20px;
+            line-height: 50px;
+            list-style-type: none;
+            padding-left: 20px;  
+        }
+        main{
+            text-align: center;
+        }
+        footer {
+            background-color: black;
+            height: 50px;
+            width: 100%;
+            position: fixed;
+            bottom: 0px;
+        }
+    </style>
+</head>
+    
+    <body>
+        <header>
+            <ul>
+                <li>トップ</li>
+                <li>プロフィール</li>
+                <li>D.IBlogについて</li><li>登録フォーム</li>
+                <li>問い合わせ</li>
+                <li>その他</li>
+            </ul>
+        </header>
+        
+        <main>
+            <h1>アカウント登録画面</h1>
+            <form method="post"action="regist_confirm.php">
+                <div>
+                    <label>名前（姓）　　</label>
+                    <input type="text"class="text"size="20"name="familyname">
+                </div>
+                <div>
+                    <label>名前（名）　　</label>
+                    <input type="text"class="text"size="20"name="lastname">
+                </div>
+                <div>
+                    <label>カナ（姓）　　</label>
+                    <input type="text"class="text"size="20"name="kana_family">
+                </div>
+                <div>
+                    <label>カナ（名）　　</label>
+                    <input type="text"class="text"size="20"name="kana_name">
+                </div>
+                <div>
+                    <label>メールアドレス　　</label>
+                    <input type="text"class="text"size="20"name="mail">
+                </div>
+                <div>
+                    <label>パスワード　　</label>
+                    <input type="password"name="password">
+                </div>
+                <div>
+                    <label>性別</label>
+                    <input type="radio"name="gender"value="男">男
+                    <input type="radio"name="gender"value="女">女
+                </div>
+                <div>
+                    <label>郵便番号　　</label>
+                    <input type="text"class="text"size="10"name="postalcode">
+                </div>
+                <div>
+                    <label>住所（都道府県）  </label>
+                    <select name="pre">
+                        <option value=""selected></option>
+                        <option value="1">北海道</option>
+                        <option value="2">青森県</option>
+                        <option value="3">岩手県</option>
+                        <option value="4">宮城県</option>
+                        <option value="5">秋田県</option>
+                        <option value="6">山形県</option>
+                        <option value="7">福島県</option>
+                        <option value="8">茨城県</option>
+                        <option value="9">栃木県</option>
+                        <option value="10">群馬県</option>
+                        <option value="11">埼玉県</option>
+                        <option value="12">千葉県</option>
+                        <option value="13">東京都</option>
+                        <option value="14">神奈川県</option>
+                        <option value="15">新潟県</option>
+                        <option value="16">富山県</option>
+                        <option value="17">石川県</option>
+                        <option value="18">福井県</option>
+                        <option value="19">山梨県</option>
+                        <option value="20">長野県</option>
+                        <option value="21">岐阜県</option>
+                        <option value="22">静岡県</option>
+                        <option value="23">愛知県</option>
+                        <option value="24">三重県</option>
+                        <option value="25">滋賀県</option>
+                        <option value="26">京都府</option>
+                        <option value="27">大阪府</option>
+                        <option value="28">兵庫県</option>
+                        <option value="29">奈良県</option>
+                        <option value="30">和歌山県</option>
+                        <option value="31">鳥取県</option>
+                        <option value="32">島根県</option>
+                        <option value="33">岡山県</option>
+                        <option value="34">広島県</option>
+                        <option value="35">山口県</option>
+                        <option value="36">徳島県</option>
+                        <option value="37">香川県</option>
+                        <option value="38">愛媛県</option>
+                        <option value="39">高知県</option>
+                        <option value="40">福岡県</option>
+                        <option value="41">佐賀県</option>
+                        <option value="42">長崎県</option>
+                        <option value="43">熊本県</option>
+                        <option value="44">大分県</option>
+                        <option value="45">宮崎県</option>
+                        <option value="46">鹿児島県</option>
+                        <option value="47">沖縄県</option>
+                    </select>
+                </div>
+                <div>
+                    <label>住所（市区町村）　　</label>
+                    <input type="text"class="text"size="20"name="zyuusyo">
+                </div>
+                <div>
+                    <label>住所（番地）  </label>
+                    <input type="text"class="text"size="20"name="banchi">
+                </div>
+                <div>
+                    <label>アカウント権限　　</label>
+                    <select name="">
+                        <option value="一般"selected>一般</option>
+                        <option value="管理者">管理者</option>
+                    </select>
+                </div>
+                <br><br>
+                <button class=""type="button">確認する</button>
+            </form>
+        </main>
+        
+        <footer>
+        </footer>
+    </body>
+</html>
